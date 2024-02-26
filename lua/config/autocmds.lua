3c757775b9b51fdfecd1ea156eefb6e51497b980
Jake Larson <jake.j.larson@gmail.com>
2024-02-25 20:16:25 -0600

    Initial commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,12 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+-- Fix conceallevel for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("wrap_spell", { clear = true }),
+    pattern = { "markdown" },
+    callback = function()
+        vim.opt_local.conceallevel = 0
+    end,
+})
